3f7de5c1f5ec9192143135007f56edc512ed0bbf
Marijn Kok <marijn.kok@mobilewater.nl>
2017-02-28 14:38:47 +0100

    added method to parse text to an array. added method to parse the array to json

--- src/Pdf.php
+++ src/Pdf.php
@@ -40,6 +40,29 @@ class Pdf
         return trim($process->getOutput(), " \t\n\r\0\x0B\x0C");
     }
 
+    public function array() : array
+    {
+        $process = new Process("{$this->binPath} '{$this->pdf}' -");
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new CouldNotExtractText($process);
+        }
+
+        $pages = explode("\f", $process->getOutput());
+        foreach($pages as $key => $page)
+        {
+            $pages[$key] = explode("\n", $page);
+        }
+
+        return $pages;
+    }
+
+    public function json() : string
+    {
+        return json_encode($this->array());
+    }
+
     public static function getText(string $pdf, string $binPath = null) : string
     {
         return (new static($binPath))
